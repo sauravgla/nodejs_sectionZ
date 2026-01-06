6b6bb35a9e90cc4909c825d97eeb333fe430e067
Saurav8005 <sauravchauhan8005@gmail.com>
2026-01-06 09:27:23 +0530

    pratice set

---
+++ practice1.js
@@ -0,0 +1,10 @@
+//write a for palindrome string
+
+// declare a string
+// check in the loop , if the reverse string is same
+// if same , print output as "yes it is palindrome"
+// if not , print output as "not palindrome"
+// to print in javascript you need console.log()
+
+let str = "Hello"
+console.log(str)
